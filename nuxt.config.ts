be8183f7b66720b4e844e2b00f3e8b325338bee3
Hausk <dbessa.jonathan@gmail.com>
2024-10-03 17:28:04 +0200

    Update Hub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-07-30',
   future: { compatibilityVersion: 4 },
   modules: [
-    '@nuxthub/core',
     '@nuxt/ui',
     'nuxt-auth-utils',
     '@nuxt/eslint',
     '@nuxtjs/tailwindcss',
     '@nuxt/icon'
   ],
-  hub: {
-    database: true
-  },
   // Development config
   eslint: {
     config: {
