a9bdb8578655bdbe32d5dc281741a82d19be3fc0
Hausk <dbessa.jonathan@gmail.com>
2024-10-15 11:36:30 +0200

    chore(login): deleted login form.

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,39 +1,18 @@
 <script setup lang="ts">
 import type { NuxtError } from '#app'
-import type { FormError, FormErrorEvent, FormSubmitEvent } from '#ui/types'
 
-const state = reactive({
-  email: undefined,
-  password: undefined
-})
 const errorMessage = ref('')
-const validate = (state: any): FormError[] => {
-  const errors = []
-  if (!state.email) errors.push({ path: 'email', message: 'Required' })
-  if (!state.password) errors.push({ path: 'password', message: 'Required' })
-  return errors
-}
 const errorUrl = useRoute().query.error as string
 if (errorUrl === 'unauthorized') {
   const toast = useToast()
   errorMessage.value = 'Vous n\'êtes pas autorisé à accéder à cette page.'
   toast.add({
-    id: 1,
+    id: '1',
     title: 'Erreur',
     description: errorMessage.value,
     color: 'red',
     timeout: 10000
   })
-}
-async function onSubmit(event: FormSubmitEvent<any>) {
-  // Do something with data
-  console.log(event.data)
-}
-
-async function onError(event: FormErrorEvent) {
-  const element = document.getElementById(event.errors[0].id)
-  element?.focus()
-  element?.scrollIntoView({ behavior: 'smooth', block: 'center' })
 }
 
 useSeoMeta({
@@ -81,41 +60,6 @@ definePageMeta({
             block
             variant="solid"
           />
-          <UDivider label="OU" />
-          <UForm
-            :validate="validate"
-            :state="state"
-            class="space-y-4"
-            @submit="onSubmit"
-            @error="onError"
-          >
-            <UFormGroup
-              label="Email"
-              name="email"
-            >
-              <UInput v-model="state.email" />
-            </UFormGroup>
-
-            <UFormGroup
-              label="Password"
-              name="password"
-            >
-              <UInput
-                v-model="state.password"
-                class="mb-8"
-                type="password"
-              />
-            </UFormGroup>
-
-            <UButton
-              type="submit"
-              block
-              class="mt-4"
-              size="xl"
-            >
-              Confirmer
-            </UButton>
-          </UForm>
         </div>
       </UCard>
     </UContainer>
